dfc115c555e28e646db9e1b99255d94aa48bbfbd
KozhaAkhmet <kozha24082001@gmail.com>
2023-09-25 19:45:31 +0300

    Seperated code for debugging

--- ArduinoSerial/ArduinoSerial.ino
+++ ArduinoSerial/ArduinoSerial.ino
@@ -7,10 +7,103 @@
 
 AF_DCMotor motor[4](1);
 #define DEFAULT_SPEED 120
+#define DEBUG false
 
 String data;
 int global_speed;
+int motor_states[4] {};
 
+#if DEBUG == false //======================
+
+void setup() {
+  Serial.begin(9600);           
+  Serial.println("Started!");
+  
+  global_speed = DEFAULT_SPEED;
+
+  for(int i = 0 ; i < 4 ; i++){
+    motor[i] = AF_DCMotor(i+1);
+    motor[i].setSpeed(global_speed);
+    motor[i].run(RELEASE);
+    motor[i].run(FORWARD);
+  }  
+  stop();
+}
+
+void loop() {
+  if(Serial.available() > 0){
+
+    static StaticJsonDocument<50> json_doc;
+    const auto serial_data = deserializeJson(json_doc,Serial);
+
+    int global_speed = json_doc["speed"];
+    String c =  json_doc["direction"]; 
+
+    if( c.equals("w") ){
+      forward(global_speed);
+    }else
+    if( c.equals("s") ){
+      backward(global_speed);
+    }else
+    if( c.equals("a") ){
+      turn_left(global_speed);
+    }else
+    if( c.equals("d") ){
+      turn_right(global_speed);
+    }else
+    if( c.equals("p") ){
+      stop();
+    }
+
+    //TODO create motor state json serialization.
+    Serial.println(" ");
+  }
+}
+void set_speed_to_motors(int speed){
+   global_speed = speed;
+}
+
+void forward(int speed){
+  for(int i = 0 ; i < 4 ; i++){
+    motor[i].setSpeed(speed);  
+    motor[i].run(FORWARD);
+    motor_states[i]++;
+  }
+}
+void backward(int speed){
+  for(int i = 0 ; i < 4 ; i++){
+    motor[i].setSpeed(speed);  
+    motor[i].run(BACKWARD);
+  }
+}
+void turn_left(int speed){
+  for(int i = 0 ; i < 4 ; i++)
+    motor[i].setSpeed(speed);
+  
+  motor[0].run(BACKWARD);
+  motor[2].run(BACKWARD);
+
+  motor[1].run(FORWARD);
+  motor[3].run(FORWARD);
+}
+void turn_right(int speed){
+  for(int i = 0 ; i < 4 ; i++)
+    motor[i].setSpeed(speed);
+  
+  motor[1].run(BACKWARD);
+  motor[3].run(BACKWARD);
+
+  motor[0].run(FORWARD);
+  motor[2].run(FORWARD);
+}
+
+void stop(){
+  for(int i = 0 ; i < 4 ; i++){
+    motor[i].setSpeed(0);  
+  }
+}
+
+#else  //=============== DEBUG MODE =====================
 
 void setup() {
   Serial.begin(9600);           
@@ -57,6 +150,7 @@ void loop() {
     if( c.equals("t") ){
       test_motors();
     }
+    Serial.println(" ");
   }
 }
 void set_speed_to_motors(int speed){
@@ -66,22 +160,39 @@ void set_speed_to_motors(int speed){
 void forward(int speed){
   Serial.println("Moving forward!");
   for(int i = 0 ; i < 4 ; i++){
-    motor[i].run(FORWARD);
     motor[i].setSpeed(speed);  
+    motor[i].run(FORWARD);
+    motor_states[i]++;
   }
 }
 void backward(int speed){
   Serial.println("Moving backward!");
   for(int i = 0 ; i < 4 ; i++){
-    motor[i].run(BACKWARD);
     motor[i].setSpeed(speed);  
+    motor[i].run(BACKWARD);
   }
 }
 void turn_left(int speed){
   Serial.println("Turning left!");
+  for(int i = 0 ; i < 4 ; i++)
+    motor[i].setSpeed(speed);
+  
+  motor[0].run(BACKWARD);
+  motor[2].run(BACKWARD);
+
+  motor[1].run(FORWARD);
+  motor[3].run(FORWARD);
 }
 void turn_right(int speed){
   Serial.println("Turning right!");
+  for(int i = 0 ; i < 4 ; i++)
+    motor[i].setSpeed(speed);
+  
+  motor[1].run(BACKWARD);
+  motor[3].run(BACKWARD);
+
+  motor[0].run(FORWARD);
+  motor[2].run(FORWARD);
 }
 
 void stop(){
@@ -109,3 +220,4 @@ void test_motors(){
     }
   }
 }
+#endif
